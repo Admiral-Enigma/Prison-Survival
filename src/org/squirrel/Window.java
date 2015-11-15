d024253361f2dce0f941c7869413104a7c94beee
Alexander <MrRandomgamerdkHD@gmail.com>
2015-11-15 16:41:12 +0100

    Added fullscreen

--- src/org/squirrel/Window.java
+++ src/org/squirrel/Window.java
@@ -1,18 +1,35 @@
 package org.squirrel;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Window {
 	public static final double version = 0.1;
 	public static final String title = "Prison Survival V"+version;
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
-	
+	static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[1];
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setTitle(title);
 		frame.add(new Game());
+		int answer = JOptionPane.showConfirmDialog (frame, "Fullscreen?");
+	    if (answer == JOptionPane.YES_OPTION) {
+			device.setFullScreenWindow(frame);
+			System.out.println("0");
+		}else if (answer == JOptionPane.NO_OPTION) {
+			frame.setSize(WIDTH, HEIGHT);
+			frame.setLocationRelativeTo(null);
+
+		}else{
+			frame.setSize(WIDTH, HEIGHT);
+			frame.setLocationRelativeTo(null);
+		}
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
